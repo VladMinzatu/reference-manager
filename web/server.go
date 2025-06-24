c84d78ac79af0089e9d3e68410557971deb3249f
Vlad Minzatu <minzatu.vlad@gmail.com>
2025-06-24 08:22:09 +0200

    this is a test

--- web/server.go
+++ web/server.go
@@ -6,14 +6,6 @@ import (
 
 /*
 Next steps:
-- Add a form to add new categories
-- Add a form to add new references
-- Add a form to edit existing references
-- Button to delete references with confirmation
-- Interactivity to reorder categories and references
-- Optional: implement lazy loading / infinite scroll for references using hx-trigger="revealed"
-- Optional: Add a search bar
-
 - DDD review and refactor: service -> aggregates and how aggregate roots integrate the repository
 
 - Implement error handling for the server start.
